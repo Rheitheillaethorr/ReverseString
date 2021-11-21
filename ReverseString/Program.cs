85d55917dea71e36d3120c76a69b9f572d55d0fe
Rheitheillaethor <minegocu3@gmail.com>
2021-11-21 03:41:38 +0100

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me string");
+            string givenString = Console.ReadLine();
+            string reversedString = "";
+            for (int i = givenString.Length-1; i>=0 ;i--)
+            {
+                reversedString = reversedString+givenString[i];
+            }
+            Console.WriteLine(reversedString);
+        }
+    }
+}
